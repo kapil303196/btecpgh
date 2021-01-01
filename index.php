c4b072a99801f45a2ac77f1e71e0d478761df682
kapil upadhyay <kapil303196@gmail.com>
2021-01-01 22:00:11 +0530

    header footer separated

---
+++ index.php
@@ -0,0 +1,683 @@
+<?php include 'header.php';?>
+<div class="bannerinfo">
+
+			<div class="container">
+
+				<div class="col-md-5 bannergrid">
+
+					<div class="top">
+
+						<h5>Education is a Powerful Weapon</h5>
+
+						<h2>The Future Experience</h2>
+
+					</div>
+
+					<div class="bottom">
+
+						<div class="col-md-6 bannergrid1 clr">
+
+							
+
+							<h4>Blockchain</h4>
+							<h4>Distributed Ledger</h4>
+							
+
+							<div class="clearfix"></div>	
+				
+
+						</div>
+	
+						<div class="col-md-6 bannergrid1 clr1">
+
+						
+
+							<h4>Emerging Technology</h4>	
+                            <h4>education</h4>							
+
+							<div class="clearfix"></div>			
+
+
+						</div>
+
+						<div class="clearfix"></div>
+
+						<div class="col-md-6 bannergrid1 clr2">
+
+					
+
+							<h4>Workshop</h4>	
+<h4>Lectures</h4>								
+
+							<div class="clearfix"></div>			
+
+							
+
+						</div>
+
+						<div class="col-md-6 bannergrid1 clr3">
+
+						
+
+							<h4>Classes</h4>		
+	<h4>Training</h4>								
+
+							<div class="clearfix"></div>			
+
+							
+
+						</div>
+
+						<div class="clearfix"></div>
+
+						<div class="col-md-6 bannergrid1 clr4">
+
+						
+
+							<h4>Implementation</h4>								
+<h4>Strategy</h4>								
+
+							<div class="clearfix"></div>			
+
+						
+						</div>
+
+						<div class="col-md-6 bannergrid1 clr5">
+
+					
+
+							<h4>Artificial Intelligence</h4>	
+<h4>Pittsburgh</h4>								
+
+							<div class="clearfix"></div>		
+
+							
+
+						</div>
+
+						<div class="clearfix"></div>
+
+					</div>
+
+					<div class="clearfix"></div>	
+
+				</div>
+
+			</div>
+
+		</div>
+
+</div>
+
+<!-- //banner -->
+
+
+
+<!-- welcome -->
+<br>
+<div class="container">  
+<div class="col-md-12 welcome-w3right">
+<h3 class="heading">Proactive Education</h3>
+	<p align="center">Every life event presents an educational opportunity or a “teaching moment”. At BTEC, we provide proactive education in emerging technology putting our students at the forefront of your industry. Once our participants are introduced to emerging technology they will understand how to implement it into their life, business and career.</p>
+<br>
+</div></div>
+
+<div class="welcome">
+
+	<div class="container">  
+
+		<div class="welcome-grids"> 
+
+			<div class="col-md-6 welcome-w3right">
+
+				<img src="images/welcome.jpg" class="img-responsive" alt="" />
+
+				<div class="wthree-model-video">
+
+					<a class="#" data-toggle="modal" data-target="#myModal1">
+
+						<span class="glyphicon glyphicon-play-circle" aria-hidden="true"></span>
+
+					</a>
+
+				</div>
+
+			</div>
+
+			<div class="col-md-6 welcome-w3left">
+
+				<h3>BTEC: Blockchain Technology Education Center</h3>
+
+				<h4>Building Equity through emerging technology.</h4>
+
+				<p>Change is constant. 
+
+At BTEC we introduce emerging technology to the communities.
+Through workshops, discussions, webinars, research and test labs ideas of an emerging world will be explored and introduced.
+
+Founder Jonnet Solomon saw a need for emerging technology education when consulting on a Blockchain Technology Project. The issue was not the technology, it was the fact that local communities were unaware that innovation and emerging technologies was happening. Essentially, there was no buy-in. The next step was to educate the community about Blockchain which led to introducing all Emerging Technology.</p>
+
+				<div class="readmore-w3">
+
+					<a class="readmore" href="http://new.renownsoundlightsanddjs.com/about.html" data-toggle="modal" data-target="#myModal1">View More</a>
+
+				</div>
+
+			</div>
+
+			<div class="clearfix"></div>
+
+		</div>
+
+	</div>
+
+</div>
+
+<!-- //welcome -->
+
+	
+
+<!-- book1 -->
+
+<div class="book  book1 jarallax">
+
+	<div class="container">
+
+	<h3>Education is the most powerful weapon which you can use to </h3>
+
+	<h3>change the world.</h3>
+
+	<p>The function of education is to teach one to think intensively and to think critically. Intelligence plus character - that is the goal of true education. Education is an enlightening experience</p>
+
+
+
+	</div>
+
+</div>
+
+<!-- //book1 -->
+
+
+
+<!-- Education -->
+
+
+
+<div class="container">  
+<div class="education">
+<div class="col-md-12 welcome-w3right">
+<h3 class="heading">Emerging Technologies</h3>
+	<p align="center">Emerging Technologies is any new technology being developed or will be developed for market use. Any Digital Strategy involves the recognition of emerging technologies and the impact such technologies will have on your life. Even if you do not fully understand such technologies, knowing what is being developed for future use is a critical part of any plan or strategy. The following are the fastest growing emerging technologies:</p>
+<br>
+
+<p align="center">Currently, we are focused on Distributed Ledger, Digitization and Storage. Our goal however, is to introduce communities to all emerging innovative technologies.
+</p>
+
+</div></div>
+
+	</div>
+
+<br>	
+<br><br>
+
+<div class="container">  
+<div class="col-md-4">
+<span class="glyphicon glyphicon-adjust" aria-hidden="true"></span>
+<h3 align="center">Distributed Ledger</h3><br>
+<h5 align="center">Understanding game changing technology</h5>
+
+</div>
+<div class="col-md-4">
+<span class="glyphicon glyphicon-adjust" aria-hidden="true"></span>
+<h3 align="center">Digitization</h3><br>
+<h5 align="center">Paper to Electronic</h5>
+
+</div>
+<div class="col-md-4">
+<span class="glyphicon glyphicon-adjust" aria-hidden="true"></span>
+<h3 align="center">Mass Storage</h3><br>
+<h5 align="center">Efficiency of Storage</h5>
+
+</div>
+
+</div><br><br>
+<div class="container">  
+<div class="col-md-12">
+<p align="center">Although full understanding of all emerging technologies is not necessary or realistic, several of the following emerging technologies will most certainly have an impact on your business:</p></div></div>
+<br><br>
+
+	<div class="container">  
+
+		<div class="col-md-4 eduleft">
+
+			<div class="left1">
+
+				<h3>Distributed Ledger</h3>
+
+				<p>Understanding game changing technology</p>
+
+				<span class="glyphicon glyphicon-adjust" aria-hidden="true"></span>
+
+			</div>
+
+			<div class="left1">
+
+				<h3>Artificial Intelligence</h3>
+
+				<p>Academic Attainment</p>
+
+				<span class="glyphicon glyphicon-adjust" aria-hidden="true"></span>
+
+			</div>
+
+			<div class="left1">
+
+				<h3>Machine Learning</h3>
+
+				<p>Practical Understanding</p>
+
+				<span class="glyphicon glyphicon-adjust" aria-hidden="true"></span>
+
+			</div>
+
+			<div class="left1">
+
+				<h3>3D Printing</h3>
+
+				<p>To Exchange Ideas</p>
+
+				<span class="glyphicon glyphicon-adjust" aria-hidden="true"></span>
+
+			</div>
+			
+			<br><br><br><br>
+			<div class="left1">
+
+				<h3>Augmented Reality</h3>
+
+				<p>To Exchange Ideas</p>
+
+				<span class="glyphicon glyphicon-adjust" aria-hidden="true"></span>
+
+			</div>
+			<div class="left1">
+
+				<h3>Moore’s Law</h3>
+
+				<p>To Exchange Ideas</p>
+
+				<span class="glyphicon glyphicon-adjust" aria-hidden="true"></span>
+
+			</div>
+
+		</div>
+		
+		
+		<div class="col-md-4 eduleft">
+
+			<div class="left1">
+
+				<h3>Digitization</h3>
+
+				<p>Skills Through Study</p>
+
+				<span class="glyphicon glyphicon-adjust" aria-hidden="true"></span>
+
+			</div>
+
+			<div class="left1">
+
+				<h3>IoT</h3>
+
+				<p>Academic Attainment</p>
+
+				<span class="glyphicon glyphicon-adjust" aria-hidden="true"></span>
+
+			</div>
+
+			<div class="left1">
+
+				<h3>Mass Storage</h3>
+
+				<p>Practical Understanding</p>
+
+				<span class="glyphicon glyphicon-adjust" aria-hidden="true"></span>
+
+			</div>
+
+			<div class="left1">
+
+				<h3>Exocortex</h3>
+
+				<p>To Exchange Ideas</p>
+
+				<span class="glyphicon glyphicon-adjust" aria-hidden="true"></span>
+
+			</div>
+			<br><br><br><br>
+			<div class="left1">
+
+				<h3>Li-Fi</h3>
+
+				<p>To Exchange Ideas</p>
+
+				<span class="glyphicon glyphicon-adjust" aria-hidden="true"></span>
+
+			</div>
+			
+			<div class="left1">
+
+				<h3>Machine Translation</h3>
+
+				<p>To Exchange Ideas</p>
+
+				<span class="glyphicon glyphicon-adjust" aria-hidden="true"></span>
+
+			</div>
+
+		</div>
+		
+		
+		<div class="col-md-4 eduleft">
+
+			<div class="left1">
+
+				<h3>Biotec Vision</h3>
+
+				<p>Skills Through Study</p>
+
+				<span class="glyphicon glyphicon-adjust" aria-hidden="true"></span>
+
+			</div>
+
+			<div class="left1">
+
+				<h3>Moco Learning (Mobile Collaboration)</h3>
+
+				<p>Academic Attainment</p>
+
+				<span class="glyphicon glyphicon-adjust" aria-hidden="true"></span>
+
+			</div>
+
+			<div class="left1">
+
+				<h3>Nanoradio</h3>
+
+				<p>Practical Understanding</p>
+
+				<span class="glyphicon glyphicon-adjust" aria-hidden="true"></span>
+
+			</div>
+
+			<div class="left1">
+
+				<h3>Quantum Computing</h3>
+
+				<p>To Exchange Ideas</p>
+
+				<span class="glyphicon glyphicon-adjust" aria-hidden="true"></span>
+
+			</div>
+			
+
+		</div>
+		
+		
+		</div></div>
+
+		<!--<div class="col-md-8 eduright">
+
+			<div class="right1">
+
+				<h3>facilitating learning</h3>
+
+				<p></p>
+
+			</div>
+
+			<div class="right1">
+
+				<h3>acquisition of knowledge</h3>
+
+				<p></p>
+
+			</div>
+
+			<div class="right1">
+
+				<h3>teaching Subject</h3>
+
+				<p></p>
+
+			</div>
+
+			<div class="right1">
+
+				<h3>Group discussion</h3>
+
+				<p></p>
+
+			</div>
+
+		</div>
+
+		<div class="clearfix"></div>
+
+	</div>
+
+</div>
+
+<!-- //Education -->
+
+
+
+<!-- book -->
+
+<div class="book jarallax">
+
+	<div class="container">
+
+	<h3>Our vision is to build equity with knowledge</h3>
+
+	<h3>Introduction to Emerging Technology</h3>
+
+	<p>You Don’t Know What You Don’t Know</p>
+	<p>Our seminars are geared to introduce participants to technologies that are not otherwise presented in daily life but are nonetheless taking place. Knowing the existence of the technology will cause our students to at least pay attention and perhaps even spark interest in a deeper knowledge of a technology. Once exposed, students can see potential changes to their chosen industry and plan their accordingly.</p>
+	
+
+		<div class="readmore-w3">
+
+			<ul>
+
+				<li><a class="readmore" href="#"  data-toggle="modal" data-target="#myModal2">Get Started</a></li>
+
+				<li><a class="readmore" href="#"  data-toggle="modal" data-target="#myModal3">Register</a></li>
+
+			</ul>
+
+		</div>
+
+	</div>
+
+</div>
+
+<!-- //book -->
+
+
+
+<!-- clients-->
+
+<!--	<div class="clients" id="clients">
+
+		<div class="container">
+
+			<h3 class="heading">Build Equiity</h3>
+
+			<div class="wmuSlider example1 animated wow slideInUp" data-wow-delay=".5s">
+
+					<div class="wmuSliderWrapper">
+
+						<article style="position: absolute; width: 100%; opacity: 0;"> 
+
+							<div class="banner-wrap">
+
+								<div class="col-md-4 client-grids">
+
+									<p></p>
+
+									<div class="col-md-5 c-img">
+
+										<img src="images/c1.jpg"  alt="" />								
+
+									</div>
+
+									<div class="col-md-7 c-info">
+
+										<h4>Jackie</h4>
+
+										<h5>student1</h5>
+
+									</div>
+
+									<div class="clearfix"></div>
+
+								</div>
+
+								<div class="col-md-4 client-grids">
+
+									<p></p>
+
+									<div class="col-md-5 c-img">
+
+										<img src="images/c2.jpg"  alt="" />								
+
+									</div>
+
+									<div class="col-md-7 c-info">
+
+										<h4>Charlize</h4>
+
+										<h5>student2</h5>
+
+									</div>
+
+									<div class="clearfix"></div>
+
+								</div>
+
+								<div class="col-md-4 client-grids">
+
+									<p></p>
+
+									<div class="col-md-5 c-img">
+
+										<img src="images/c3.jpg"  alt="" />								
+
+									</div>
+
+									<div class="col-md-7 c-info">
+
+										<h4>Student1</h4>
+
+										<h5>student3</h5>
+
+									</div>
+
+									<div class="clearfix"></div>
+
+								</div>
+
+								<div class="clearfix"></div>
+
+							</div>
+
+						</article>
+
+						<article style="position: absolute; width: 100%; opacity: 0;"> 
+
+							<div class="banner-wrap">
+
+								<div class="col-md-4 client-grids">
+
+									<p></p>
+
+									<div class="col-md-5 c-img">
+
+										<img src="images/c4.jpg"  alt="" />								
+
+									</div>
+
+									<div class="col-md-7 c-info">
+
+										<h4>Daniel</h4>
+
+										<h5>student4</h5>
+
+									</div>
+
+									<div class="clearfix"></div>
+
+								</div>
+
+								<div class="col-md-4 client-grids">
+
+									<p></p>
+
+									<div class="col-md-5 c-img">
+
+										<img src="images/c5.jpg"  alt="" />								
+
+									</div>
+
+									<div class="col-md-7 c-info">
+
+										<h4>Johnson</h4>
+
+										<h5>student5</h5>
+
+									</div>
+
+									<div class="clearfix"></div>
+
+								</div>
+
+								<div class="col-md-4 client-grids">
+
+									<p></p>
+
+									<div class="col-md-5 c-img">
+
+										<img src="images/c6.jpg"  alt="" />								
+
+									</div>
+
+									<div class="col-md-7 c-info">
+
+										<h4>Scarllet</h4>
+
+										<h5>student6</h5>
+
+									</div>
+
+									<div class="clearfix"></div>
+
+								</div>
+
+								<div class="clearfix"></div>
+
+							</div>
+
+						</article>
+
+					</div>
+
+				</div>
+
+		</div>
+
+	</div>-->
+
+<!--//clients-->
+
+<?php include 'footer.php';?>
